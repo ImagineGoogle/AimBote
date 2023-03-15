f844dfbd395887094632bdfc43ef7f28530ad15b
ImagineGoogle <113044328+ImagineGoogle@users.noreply.github.com>
2023-03-15 17:15:58 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,12 +1,43 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local Players = game:GetService("Players")
+
 local GuiLibrary = isfile("GuiLibrary.txt") and loadstring(readfile("GuiLibrary.txt"))() or loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineGoogle/AimBote/main/GuiLibrary.lua"))()
 
 GuiLibrary.Init()
 
-GuiLibrary.CreateWindow {Name = "Combat"}
-GuiLibrary.CreateWindow {Name = "Movement"}
-GuiLibrary.CreateWindow {Name = "Render"}
-GuiLibrary.CreateWindow {Name = "Utility"}
-GuiLibrary.CreateWindow {Name = "Miscellaneous"}
+local function runFunction(func)
+    func()
+end
+
+local localPlayer = Players.LocalPlayer
+local bedwars
+
+runFunction(function() -- Knit and modules
+    local KnitGotten, KnitClient
+	repeat
+		KnitGotten, KnitClient = pcall(function()
+			return debug.getupvalue(require(localPlayer.PlayerScripts.TS.knit).setup, 6)
+		end)
+		if KnitGotten then break end
+		task.wait()
+	until KnitGotten
+	repeat task.wait() until debug.getupvalue(KnitClient.Start, 1)
+
+    bedwars = {
+        SprintController = KnitClient.Controllers.SprintController,
+    }
+end)
+
+runFunction(function() -- Windows
+    GuiLibrary.CreateWindow {Name = "Combat"}
+    GuiLibrary.CreateWindow {Name = "Movement"}
+    GuiLibrary.CreateWindow {Name = "Render"}
+    GuiLibrary.CreateWindow {Name = "Utility"}
+    GuiLibrary.CreateWindow {Name = "Miscellaneous"}
+end)
 
 GuiLibrary.CreateModule {
     Window = "Miscellaneous",
@@ -19,15 +50,61 @@ GuiLibrary.CreateModule {
     end
 }
 
-GuiLibrary.CreateModule {
-    Window = "Movement",
-    Name = "Sprint",
-    Toggleable = true,
-    Function = function(callback)
-        if callback then
-            print("Enabled")
-        else
-            print("Disabled")
+runFunction(function() -- Sprint
+    local oldSprintFunction
+
+    local Sprint
+    Sprint = GuiLibrary.CreateModule {
+        Window = "Movement",
+        Name = "Sprint",
+        Toggleable = true,
+        Function = function(callback)
+            if callback then
+                oldSprintFunction = bedwars.SprintController.stopSprinting
+                bedwars.SprintController.stopSprinting = function(...)
+                    local originalCall = oldSprintFunction(...)
+                    bedwars.SprintController:startSprinting()
+                    return originalCall
+                end
+                table.insert(Sprint.Connections, localPlayer.CharacterAdded:Connect(function(character)
+                    character:WaitForChild("Humanoid", 9e9)
+                    task.wait(0.5)
+                    bedwars.SprintController:stopSprinting()
+                end))
+                task.spawn(function()
+                    bedwars.SprintController:startSprinting()
+                end)
+            else
+                bedwars.SprintController.stopSprinting = oldSprintFunction
+                bedwars.SprintController:stopSprinting()
+            end
         end
-    end
-}
+    }
+end)
+
+runFunction(function() -- Velocity
+    local Velocity = GuiLibrary.CreateModule {
+        Window = "Combat",
+        Name = "Velocity",
+        Toggleable = true,
+        Function = function(callback)
+            if callback then
+    
+            end
+        end
+    }
+    Velocity.CreateSlider {
+        Name = "X",
+        DefaultValue = 0,
+        MaxValue = 100,
+        MinValue = 0,
+        Function = function() end
+    }
+    Velocity.CreateSlider {
+        Name = "Y",
+        DefaultValue = 0,
+        MaxValue = 100,
+        MinValue = 0,
+        Function = function() end
+    }
+end)
