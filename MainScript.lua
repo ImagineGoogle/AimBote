66572169e1eaaecede30bca7824ac83af6a6df1f
ImagineGoogle <113044328+ImagineGoogle@users.noreply.github.com>
2023-03-14 23:05:04 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,7 +1,33 @@
-local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineGoogle/AimBote/main/GuiLibrary.lua"))()
+local GuiLibrary = isfile("GuiLibrary.txt") and loadstring(readfile("GuiLibrary.txt"))() or loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineGoogle/AimBote/main/GuiLibrary.lua"))()
+
+GuiLibrary.Init()
 
 GuiLibrary.CreateWindow {Name = "Combat"}
-GuiLibrary.CreateWindow {Name = "Blatant"}
+GuiLibrary.CreateWindow {Name = "Movement"}
 GuiLibrary.CreateWindow {Name = "Render"}
 GuiLibrary.CreateWindow {Name = "Utility"}
-GuiLibrary.CreateWindow {Name = "World"}
+GuiLibrary.CreateWindow {Name = "Miscellaneous"}
+
+GuiLibrary.CreateModule {
+    Window = "Miscellaneous",
+    Name = "Uninject",
+    Toggleable = false,
+    Function = function(callback)
+        if callback then
+            GuiLibrary.Uninject()
+        end
+    end
+}
+
+GuiLibrary.CreateModule {
+    Window = "Movement",
+    Name = "Sprint",
+    Toggleable = true,
+    Function = function(callback)
+        if callback then
+            print("Enabled")
+        else
+            print("Disabled")
+        end
+    end
+}
