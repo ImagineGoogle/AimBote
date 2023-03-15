3ee0727a696616165981cce0bf2a7ba7794e3825
ImagineGoogle <113044328+ImagineGoogle@users.noreply.github.com>
2023-03-14 21:53:25 -0400

    Create MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,7 @@
+local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineGoogle/AimBote/main/GuiLibrary.lua"))()
+
+GuiLibrary.CreateWindow {Name = "Combat"}
+GuiLibrary.CreateWindow {Name = "Blatant"}
+GuiLibrary.CreateWindow {Name = "Render"}
+GuiLibrary.CreateWindow {Name = "Utility"}
+GuiLibrary.CreateWindow {Name = "World"}
